7c7c53bb840a8762f9312632d04c3c5b88d0f740
Anshul <anshulwagadre@gmail.com>
2020-07-27 21:03:43 +0530

    added core materials

--- src/app/shared/materials/materials.module.ts
+++ src/app/shared/materials/materials.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   exports: [MatButtonModule],
 })
 export class MaterialsModule {}
+
+@NgModule({
+  exports: [MatSnackBarModule],
+})
+export class CoreMaterial {}
