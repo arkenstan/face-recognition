5e40b3ea2d052be1948c6f98de42447a2a2561e1
Anshul <anshulwagadre@gmail.com>
2020-07-28 01:47:26 +0530

    added loader invocations

--- src/app/face-recognition/containers/main/main.component.ts
+++ src/app/face-recognition/containers/main/main.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder } from '@angular/forms';
 
 import { FaceapiService } from '../../services/faceapi.service';
 import { NotificationService } from 'src/app/core/services/notification.service';
+import { LoaderService } from 'src/app/core/services/loader.service';
 
 @Component({
   selector: 'app-main',
@@ -10,13 +11,15 @@ import { NotificationService } from 'src/app/core/services/notification.service'
   styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
-  @ViewChild('idImage') idImage: ElementRef<HTMLInputElement>;
-  @ViewChild('profileImage') profileImage: ElementRef<HTMLInputElement>;
-
   imageForm = this.fb.group({
     idCardImage: [null],
     profileImage: [null],
   });
+
+  uiState = {
+    idInputDisabled: true,
+    profileInputDisabled: true,
+  };
 
   images = {
     identity: null,
@@ -27,12 +30,14 @@ export class MainComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private faceApi: FaceapiService,
-    private notificationService: NotificationService
+    private notificationService: NotificationService,
+    private loaderService: LoaderService
   ) {}
 
   async ngOnInit(): Promise<void> {
+    this.loaderService.openLoader('Loading models');
     await this.faceApi.loadModels();
-    console.log('Models loaded');
+    this.loaderService.closeLoader();
   }
 
   readFile(file: File) {
@@ -47,8 +52,10 @@ export class MainComponent implements OnInit {
       let reader = this.readFile(file);
       reader.onload = async (_event) => {
         let result = reader.result.toString();
+        this.loaderService.openLoader('Detecting Face');
         let success = await this.faceApi.calculateIdentityDescriptor(result);
         this.images.identity = result;
+        this.loaderService.closeLoader();
       };
     } else {
       this.notificationService.notify({
@@ -64,11 +71,13 @@ export class MainComponent implements OnInit {
       let reader = this.readFile(file);
       reader.onload = async (_event) => {
         let result = reader.result.toString();
+        this.loaderService.openLoader('Detecting Face');
         let success = await this.faceApi.calculateProfileDescriptor(result);
         if (success) {
           this.images.profile = result;
           this.getLikeness();
         }
+        this.loaderService.closeLoader();
       };
     } else {
       this.notificationService.notify({
