b23bc53fffe5aa063a6c8ec66b46b2d43035226a
Anshul <anshulwagadre@gmail.com>
2020-07-28 00:55:48 +0530

    added main module to declaration

--- src/app/face-recognition/face-recognition.module.ts
+++ src/app/face-recognition/face-recognition.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
-import { SharedModule } from '../shared/shared.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { FaceRecognitionMaterials } from 'src/app/shared/materials/materials.module';
 
 import { FaceRecognitionRoutingModule } from './face-recognition-routing.module';
+import { MainComponent } from './containers/main/main.component';
 
 @NgModule({
-  declarations: [],
-  imports: [SharedModule, FaceRecognitionRoutingModule],
+  declarations: [MainComponent],
+  imports: [
+    FaceRecognitionRoutingModule,
+    SharedModule,
+    FaceRecognitionMaterials,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class FaceRecognitionModule {}
