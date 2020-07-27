46f305b4b532a30e5d53f1cd0bbe2c637df267cd
Anshul <anshulwagadre@gmail.com>
2020-07-27 21:04:31 +0530

    imported materials module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { CoreMaterial } from '../shared/materials/materials.module';
 
 import { AppComponent } from './containers/app/app.component';
 import { NotFoundComponent } from './containers/not-found/not-found.component';
 
 @NgModule({
   declarations: [AppComponent, NotFoundComponent],
-  imports: [SharedModule],
+  imports: [SharedModule, CoreMaterial],
 })
 export class CoreModule {}
