68f7e1e2b8fdc965c9473ea32a9815164a575e37
Anshul <anshulwagadre@gmail.com>
2020-07-28 00:55:12 +0530

    added route for main component

--- src/app/face-recognition/face-recognition-routing.module.ts
+++ src/app/face-recognition/face-recognition-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { MainComponent } from './containers/main/main.component';
+
+const routes: Routes = [{ path: '', component: MainComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class FaceRecognitionRoutingModule { }
+export class FaceRecognitionRoutingModule {}
