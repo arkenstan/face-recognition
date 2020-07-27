46bd2b08cd3366fe78a941e83de70ea48a62386c
Anshul <anshulwagadre@gmail.com>
2020-07-27 21:06:30 +0530

    added notification service to app

--- src/app/core/containers/app/app.component.ts
+++ src/app/core/containers/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import {
+  NotificationService,
+  Notification,
+} from '../../services/notification.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'face-recognition';
+  notification$: Observable<Notification>;
+
+  constructor(
+    private notification: NotificationService,
+    private _snackBar: MatSnackBar
+  ) {
+    this.notification$.subscribe((res) => {
+      this.openSnackBar(res);
+    });
+  }
+
+  openSnackBar({ message, level }: Notification) {
+    this._snackBar.open(message, level, { duration: 2000 });
+  }
 }
