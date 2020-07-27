5578eca31317e9fc4707fe0cb03416dc675883c2
Anshul <anshulwagadre@gmail.com>
2020-07-28 01:47:00 +0530

    added materials for loader spinners

--- src/app/shared/materials/materials.module.ts
+++ src/app/shared/materials/materials.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   exports: [MatButtonModule],
@@ -10,7 +13,12 @@ import { MatCardModule } from '@angular/material/card';
 export class MaterialsModule {}
 
 @NgModule({
-  exports: [MatSnackBarModule],
+  exports: [
+    MatSnackBarModule,
+    MatProgressBarModule,
+    MatDialogModule,
+    MatProgressSpinnerModule,
+  ],
 })
 export class CoreMaterial {}
 
